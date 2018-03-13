a299e084dd0d0e3019554867a5811b9023edf590
Home\RVA <romain.vacquerie@gmail.com>
2018-03-13 21:57:12 +0100

    register component as directive binding interpolation logic directive

--- movies-app/src/app/app.module.ts
+++ movies-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movies/movie-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule
---
+++ movies-app/src/app/movies/movie-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'movies',
+    templateUrl: './movie-list.component.html'
+})
+export class MovieListComponent{
+    pageTitle: string = 'Upcoming';
+    movies: any[] = [
+        {
+            "releaseDate": "March 19, 2016",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "price": 19.95,
+            "starRating": 3.2,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+        },
+        {
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+        } 
+    ];
+}
