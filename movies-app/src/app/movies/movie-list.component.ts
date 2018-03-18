e44ba6c7a52605bba97b6f68fbf89507b7a7a5b0
Zied <bentaharzied@gmail.com>
2018-03-18 13:36:16 +0100

    Fixing movies binding

--- movies-app/src/app/movies/movie-list.component.ts
+++ movies-app/src/app/movies/movie-list.component.ts
@@ -1,42 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { IMovie, IData } from './movie';
-import { MovieService } from './movie.service';
+import { Component, OnInit } from "@angular/core";
+import { IMovie, IData } from "./movie";
+import { MovieService } from "./movie.service";
 
 @Component({
-    selector: 'app-movie-list',
-    templateUrl: './movie-list.component.html'
+  selector: "app-movie-list",
+  templateUrl: "./movie-list.component.html"
 })
 export class MovieListComponent implements OnInit {
-    _listFilter: string;
-    pageTitle = 'Upcoming';
-    filteredMovies: IMovie[];
-    result: IData;
+  _listFilter: string;
+  pageTitle = "Upcoming";
+  filteredMovies: IMovie[];
+  result: IData;
 
-    constructor(private _movieService: MovieService) {
-    }
+  constructor(private _movieService: MovieService) {}
 
-    get listFilter(): string {
-        return this._listFilter;
-    }
-    set listFilter(value: string) {
-        this._listFilter = value;
-        this.filteredMovies = this._listFilter ? this.filtered(this.listFilter) : this.result.results;
-    }
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredMovies = this._listFilter
+      ? this.filtered(this.listFilter)
+      : this.result.results;
+  }
 
-    filtered(filterBy: string): IMovie[] {
-        filterBy = filterBy.toLocaleLowerCase();
-        return this.result.results.filter((e: IMovie) =>
-            e.title.toLocaleLowerCase().indexOf(filterBy) !== -1);
-    }
+  filtered(filterBy: string): IMovie[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.result.results.filter(
+      (e: IMovie) => e.title.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
-    ngOnInit(): void {
-        this._movieService.getUpcomingMovies(1).subscribe(e => {
-            this.result = e;
-            this.filteredMovies = this.result.results
-        });
-    }
+  ngOnInit(): void {
+    this._movieService.getUpcomingMovies(1).subscribe(e => {
+      this.result = e;
+      this.filteredMovies = this.result.results;
+      console.log(this.filteredMovies);
+    });
+  }
 
-    onRatingClicked(message: string): void {
-        console.log(message);
-    }
+  onRatingClicked(message: string): void {
+    console.log(message);
+  }
 }
